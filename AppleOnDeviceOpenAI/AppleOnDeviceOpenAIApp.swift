3b24275d4c444a1dc6d309c5926cc4aea0245a32
Channing <channingdai@gmail.com>
2025-06-15 21:57:07 +0800

    feat: init project

---
+++ AppleOnDeviceOpenAI/AppleOnDeviceOpenAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleOnDeviceOpenAIApp.swift
+//  AppleOnDeviceOpenAI
+//
+//  Created by Channing Dai on 6/15/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleOnDeviceOpenAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleOnDeviceOpenAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleOnDeviceOpenAI
+//
+//  Created by Channing Dai on 6/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
